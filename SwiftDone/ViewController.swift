d7755dd74b784dbaa4f57f1cdd3e47a2cd899c17
Keita Ito <keitaitok@gmail.com>
2015-08-15 11:22:18 -0700

    Initial Commit

---
+++ SwiftDone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDone
+//
+//  Created by Keita on 8/15/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
